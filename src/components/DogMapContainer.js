c912a823c2c5f322ba73618be5d20c620d70765c
Zena Jun <zena.jun@gmail.com>
2019-11-22 15:08:34 -0500

    updated api access

--- src/components/DogMapContainer.js
+++ src/components/DogMapContainer.js
@@ -2,15 +2,13 @@ import React from "react";
 import DogMap from "./DogMap";
 import styled from "styled-components";
 
-const api_key = process.env.REACT_APP_GOOLE_MAP_API_KEY;
-
 class DogMapContainer extends React.Component {
   render() {
     return (
       <Map>
         <DogMap
           user={this.props.user}
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${api_key}&v=3.exp&libraries=geometry,drawing,places`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOLE_MAP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={
             <div style={{ height: `100%`, width: `100%`, margin: `0 auto` }} />
